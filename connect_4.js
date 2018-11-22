e2992655202100bab5d492149377b73a4c99790e
Jonathan Rubin <jonathanrubin@jonathans-mbp.attlocal.net>
2018-11-22 12:31:52 -0500

    added "prompt" module and asking user to pick a certain column, verifying input with regex

--- connect_4.js
+++ connect_4.js
@@ -34,7 +34,7 @@ const dropPiece = (column, piece) => {
     });
 };
 
-console.log("DROP", dropPiece(playingBoard[3], 'B'));
+console.log('DROP', dropPiece(playingBoard[3], 'B'));
 
 // this should return a copy of the board with the updated column
 replaceColumn = (board, columnIndex) => {
@@ -74,5 +74,34 @@ transformRowToColumn = (board, rowIndex) =>
     board.map(column => column[rowIndex]);
 
 console.log('transform', transformRowToColumn(playingBoard, 0));
-checkColumnForWin(transformRowToColumn(playingBoard, 0), 'B')
+checkColumnForWin(transformRowToColumn(playingBoard, 0), 'B');
 // checkColumnForWin(['B', 'R', 'B', 'B', 'B', 'B'], 'B');
+
+var prompt = require('prompt');
+var schema = {
+    properties: {
+        move: {
+            // verifying that the user enters an integer between 1 and 7
+            // i am shifting indexes up by one when asking user 1 - 7 for column numbers instead of indexes 0 - 6
+            pattern: /^([1-7])$/,
+            message: 'Choose column to drop checker in (enter number 1 - 7)',
+            required: true
+        }
+        //   password: {
+        //     hidden: true
+        //   }
+    }
+};
+
+getMove = turn => {
+    prompt.start();
+    prompt.get(schema, function(err, result) {
+        console.log('Command-line input received:');
+        console.log(`${turn} dropped in column: ${result.move}`);
+        // now I am shifting columns 1 thru 7 back to indexes 0 - 7
+        return result.move - 1;
+    });
+};
+
+const test = move('R')
+console.log(test);
