661a691032e73814fceda464832bebd728501f9c
Jonathan Rubin <jonathanrubin@jonathans-mbp.attlocal.net>
2018-11-22 09:04:39 -0500

    replace column and returning a new board

--- connect_4.js
+++ connect_4.js
@@ -1,4 +1,4 @@
-const board = [
+const playingBoard = [
     ['O', 'O', 'O', 'O', 'O', 'O'],
     ['O', 'O', 'O', 'O', 'O', 'O'],
     ['O', 'O', 'O', 'O', 'O', 'O'],
@@ -12,14 +12,16 @@ player1 = {
     piece: 'B',
     wins: 0,
     losses: 0
-}
+};
 
 player2 = {
     piece: 'R',
     wins: 0,
     losses: 0
-}
+};
 
+// this will take in an array and look from bottom up if there is a piece played
+// already and go up to next spot if there is one already
 const dropPiece = (column, piece) => {
     let landed;
     const newColumn = column.map((space, i) => {
@@ -32,6 +34,17 @@ const dropPiece = (column, piece) => {
     });
     console.log(newColumn);
     return newColumn;
-}
+};
 
-dropPiece(board[0], player1.piece)
+dropPiece(playingBoard[3], 'B')
+
+// this should return a copy of the board with the updated column
+replaceColumn = (board, columnIndex) => {
+    const newBoard = board.map((column, i) => {
+        return columnIndex === i ? dropPiece(column, player1.piece) : column;
+    });
+    console.log(newBoard);
+    return newBoard;
+};
+
+replaceColumn(playingBoard, 0);
