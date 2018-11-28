e230df12731e93be88fb723ec8ab893e948e829d
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2018-11-27 21:20:39 -0500

    add tests for methods on game component

--- react-rails/client/src/components/organisms/game.test.tsx
+++ react-rails/client/src/components/organisms/game.test.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Board } from './game.component';
+import { Game } from './game.component';
 
 describe('Board component', () => {
-    let fixture, result;
+    let fixture, result, instance, column, board, winColumn;
     beforeEach(() => {
-        fixture = <Board />;
+        fixture = <Game />;
         result = shallow(fixture);
+        column = ['B', 'B', 'B', ' ', ' ', ' '];
+        board = [column, column, column, column, column, column, column];
+        instance = result.instance();
     });
     it('should render properly', () => {
         expect(result).toBeDefined();
@@ -15,13 +18,87 @@ describe('Board component', () => {
     describe('functions', () => {
         it('changeTurn should change the currentTurn from "R" to "B" and vice versa', () => {
             result.setState({ currentTurn: 'R' });
-            const instance = result.instance();
             instance.changeTurn();
             expect(result.state().currentTurn).toEqual('B');
             instance.changeTurn();
             expect(result.state().currentTurn).toEqual('R');
         });
-        it('dropPieceInColumn should call find first blank space and replace it with the piece in currentTurn', () => {});
-        it('replaceColumn should call find first blank space and replace it with the piece in currentTurn', () => {});
+        it('dropPieceInColumn should call find first blank space and replace it with the piece in currentTurn', () => {
+            const newColumn = instance.dropPieceInColumn(column, 'R');
+            expect(newColumn).toEqual(['B', 'B', 'B', 'R', ' ', ' ']);
+        });
+        it('replaceColumn should call find first blank space and replace it with the piece in currentTurn', () => {
+            const newBoard = instance.replaceColumn(board, 0, 'B');
+            expect(newBoard).toEqual([
+                ['B', 'B', 'B', 'B', ' ', ' '],
+                column,
+                column,
+                column,
+                column,
+                column,
+                column
+            ]);
+        });
+        describe('winCheckByInterval ', () => {
+            let blankColumn, nonWinBoard, colWinBoard;
+            beforeEach(() => {
+                blankColumn = [' ', ' ', ' ', ' ', ' ', ' '];
+                winColumn = ['B', 'B', 'B', 'B', ' ', ' '];
+                nonWinBoard = [
+                    column,
+                    column,
+                    column,
+                    blankColumn,
+                    column,
+                    blankColumn,
+                    column
+                ];
+                colWinBoard = [
+                    winColumn,
+                    column,
+                    column,
+                    blankColumn,
+                    column,
+                    blankColumn,
+                    column
+                ];
+            });
+            it('should return false if win condition not met on column', () => {
+                const result = instance.winCheckByInterval(
+                    nonWinBoard,
+                    'B',
+                    1,
+                    4
+                );
+                expect(result).toBeFalsy();
+            });
+            it('should return true if win condition met on column', () => {
+                const result = instance.winCheckByInterval(
+                    colWinBoard,
+                    'B',
+                    1,
+                    4
+                );
+                expect(result).toBeTruthy();
+            });
+            it('should return true if win condition met on column', () => {
+                const result = instance.winCheckByInterval(
+                    colWinBoard,
+                    'B',
+                    1,
+                    0
+                );
+                expect(result).toBeTruthy();
+            });
+            // it('should return false if there is a win, but flatIndex doesnt ID correct', () => {
+            //     const result = instance.winCheckByInterval(
+            //         colWinBoard,
+            //         'B',
+            //         1,
+            //         0
+            //     );
+            //     expect(result).toBeTruthy();
+            // });
+        });
     });
 });
