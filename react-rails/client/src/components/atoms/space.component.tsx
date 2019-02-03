ee822a12c8e4acaf62ac9c361d9e82a2c57ba1da
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2019-02-02 22:24:47 -0500

    added box shadow to board and spaces

--- react-rails/client/src/components/atoms/space.component.tsx
+++ react-rails/client/src/components/atoms/space.component.tsx
@@ -10,6 +10,11 @@ export class Space extends Component<Props, {}> {
     render() {
         const { piece } = this.props;
         const color = piece === 'B' ? 'black' : piece === 'R' ? 'red' : 'white';
-        return <div className={`bg-${color} br-100 ma1 space`} />;
+        return (
+            <div
+                className={`bg-${color} br-100 ma1 space ${piece === ' ' &&
+                    'empty'}`}
+            />
+        );
     }
 }
